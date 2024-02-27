8a69e24fb319c8cd8c20d9fe7507aff41fded750
devTarak <tarakrahman421@gmail.com>
2024-02-28 03:50:55 +0600

    last night

--- app/heroText.js
+++ app/heroText.js
@@ -39,3 +39,4 @@ function helloText(name, ...phrases1){
 }
 helloText("typeWriterPro","Tarak Rahman", "A Programmer","also an Enginner");
 helloText("typeWriterDown","Hello Everyone, I'm a Creative Web Developer and Designer. I can design and develop any type of creative website for you by coding with HTML5, CSS3, JavaScript, Bootstrap, PHP, Python. I can design and modify any type of WordPress websites and Theme for you. Just inbox me, I'm always here for you project.");
+helloText("LanKnow"," Bangla", "English", "Hindi");
