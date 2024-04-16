92737128e3229b9cc9e65e6ea942a469d7ebfe05
devTarak <tarakrahman421@gmail.com>
2024-04-17 03:58:34 +0600

    fun added

--- app/main.js
+++ app/main.js
@@ -1,7 +1,25 @@
-function projectShow(){
-    
+function infoConsole(){
+    console.log("%c!!STOP!!","font-size:90px;color:#ff0000;");
+    console.log("%cThis is a highly secure world! If you attempt to inject anything to bring down this site, your machine's information, as well as your address and personal details, will be recorded in our directory.","font-size:30px;");
+    const messages = [
+        "%cMAC Copied...",
+        "%cIp added...",
+        "%clatitude searching...",
+        "%clatitude found!!",
+        "%clatitude adding...",
+        "%cCamera port BB564...",
+        "%clongitude searching...",
+        "%cTunel port...",
+        "%clongitude found!!",
+        "%clongitude adding..."
+    ];
+    messages.forEach(message => {
+        console.log(message, "font-size:12px;color:#33ff10;");
+    });
+  
 }
 document.onselectstart = () => {
     event.preventDefault();
     alert("You Are Hacked");
 }
+infoConsole();
